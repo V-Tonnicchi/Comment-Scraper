85640af87397f2bedfcdf79e4b9b4baee8c1e511
VT <valentino.tonnicchi@alumni.fh-aachen.de>
2025-03-17 07:43:12 +0100

    Logic for Reader layed out in Annotations

--- src/Reader.java
+++ src/Reader.java
@@ -15,9 +15,13 @@ public class Reader
         ArrayList<String> returnList = new ArrayList<>();
         Scanner scan = new Scanner(new File(src));
         // TODO: Logik für das seperieren der comments in Strings, Elemente getrennt mit komma oder Zeilenumbruch.
-
-        
-
+        //do:
+            //do:
+                //Zeilen jeweils in Strings speichern
+                //Relevante Strings in ein String speichern, getrennt mit komma oder Zeilenumbruch
+                //String in ArrayList speichern
+            //Solange nächste Line nicht '#####' ist.
+        //Solange File eine nächste Line hat
 
         return returnList;
     }
